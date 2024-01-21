430d3cc274041ddea3c1f7800f04b1328229ee13
vidchaidukh <mr.sapsan220@gmail.com>
2024-01-21 08:10:39 +0200

    display buckets, coloring, layers options

---
+++ myapp/app.js
@@ -0,0 +1,119 @@
+// Initialize leaflet.js
+var L = require('leaflet');
+let lat =  50.450001
+let lon = 30.523333
+
+let map = L.map('map').setView([lat, lon], 11);
+
+/*	Variety of base layers */
+var osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; OSM <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var osm_de = L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; OSM Deutschland <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+var osm_fr = L.tileLayer('http://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+	maxZoom: 20,
+	attribution: '&copy; OSM France | &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+var osm_hot = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; OSM Hot <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
+});
+var osm_topo = L.tileLayer('http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+var esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
+// Create base layers group object
+var baseLayers = {
+	"OSM": osm,
+	"OSM Germany": osm_de,
+	"OSM France": osm_fr,
+	"OSM Hot": osm_hot,
+	"OSm Topo": osm_topo,
+	"ESRI World Imagery": esri_WorldImagery
+};
+
+let yellowIcon = L.icon({
+    iconUrl:"style/yellow_bin.png",
+    iconSize:[40,40],
+    popupAnchor: [0, -10]
+
+   })
+let greenIcon = L.icon({
+    iconUrl:"style/green_bin.png",
+    iconSize:[40,40],
+    popupAnchor: [0, -10]
+})
+let redIcon = L.icon({
+    iconUrl:"style/red_bin.png",
+    iconSize: [40, 40],
+    popupAnchor: [0, -10]
+})
+let iconOptions = {
+    draggable:false,
+    icon:redIcon
+}
+
+const markers_json = require('./markers.json'); 
+let markers = []
+for (let mkr_ind in markers_json) {
+    mkr = markers_json[mkr_ind]
+    if (mkr['fill_status'] > 90){
+        mkr_icon = redIcon
+    }else if (mkr['fill_status'] > 60){
+        mkr_icon = yellowIcon
+    }else{
+        mkr_icon = greenIcon
+    }
+    new_marker = L.marker([mkr['lat'], mkr['lon']], {icon: mkr_icon})
+                    .bindPopup(mkr['popup'] + 
+                    '<br>Filled up ' + mkr['fill_status'] + '%'),
+    markers.push(new_marker)
+ }
+
+// Add baseLayers and overlays to layer panel
+var buckets = L.layerGroup(markers).addTo(map);
+
+var overlays = {
+	'buckets': buckets
+};
+L.control.layers(baseLayers).addTo(map);
+L.control.layers(null, overlays).addTo(map);
+
+
+
+
+function update_markers(){
+    let marker = new L.Marker([50.398845,30.5070573] , iconOptions);
+    marker.addTo(map);
+    marker.bindPopup('content').openPopup();
+    marker.addEventListener ("click", move_bucket, false);
+    function move_bucket(){
+        console.log(marker)
+        new_lat = marker['_latlng']['lat']*(0.995 + Math.random() * .010);
+        new_lon = marker['_latlng']['lng']*(0.995 + Math.random() * .010);
+        console.log(new_lat)
+        var newLatLng = new L.LatLng(new_lat, new_lon);
+        marker.setLatLng(newLatLng); 
+    }
+    while (1){
+        markers.forEach((marker) => {
+            console.log(marker)
+            new_lat = marker['_latlng']['lat']*Math.floor(Math.random() * 0.005);
+            new_lon = marker['_latlng']['lng']*Math.floor(Math.random() * 0.005);
+            var newLatLng = new L.LatLng(new_lat, new_lon);
+            marker.setLatLng(newLatLng); 
+        })
+        setTimeout(function() {
+            statement2();
+        }, 500);
+    }
+}
+//setTimeout(update_markers, 5000, "First task");
